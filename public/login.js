eda24565bf0c55546f555f6147bd04a784e255a8
josharsh <harsh.joshi.pth@gmail.com>
2020-03-28 13:18:14 +0530

    Introduce Web Admin Portal

---
+++ public/login.js
@@ -0,0 +1,8 @@
+var name=form.name.email;
+var password=form.name.pass;
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
